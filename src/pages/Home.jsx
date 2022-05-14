c1f2b60678f3e08ea2af46d3a81740d6752f6c7f
Alyssa Voccia <avoccia@hotmail.com>
2022-05-14 07:47:44 -0400

    Updated pokemon cards on home screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,26 @@ function Home() {
   const [pokemonList, setPokemonList] = useState([]);
   const [next, setNext] = useState(20);
 
+  const pokemonTypeColors = {
+    bug: "#A3CB38",
+    dragon: "#8e44ad",
+    electric: "#f1c40f",
+    fairy: "#fd79a8",
+    fighting: "#d63031",
+    fire: "#e67e22",
+    flying: "#a29bfe",
+    grass: "#16a085",
+    ground: "#fdcb6e",
+    ghost: "#a55eea",
+    ice: "#74b9ff",
+    normal: "#95a5a6",
+    poison: "#6c5ce7",
+    psychic: "#e84393",
+    rock: "#EAB543",
+    steel: "#a5b1c2",
+    water: "#3498db"
+  };
+
   const loopWithSlice = () => {
     const toShow = pokemon.slice(
       pokemonList.length,
@@ -47,26 +67,26 @@ function Home() {
           <button className="btn btnPrimary">Generate Random Pokemon</button>
         </div>
       </section>
-      <section>
-        <div className="pokemonListContainer">
+      <section className='pokemonContainer'>
+        <div className="pokemonCardContainer">
           {pokemonList && pokemonList.map(pokemon => (
-            <div className="pokemonCard" key={pokemon.id}>
+            <div className='pokemonCard' key={pokemon.name} style={{background: `radial-gradient(circle at 50% 0%, ${pokemonTypeColors[pokemon.types[0].type.name]} 36%, #FFF 36%)`}}>
               <div className="cardHeader">
-                <span>#{pokemon.id}</span>
-                <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                <span className='pokemonId'>#{pokemon.id}</span>
+                <img className='pokemonImg' src={pokemon.sprites.front_default} alt={pokemon.name} />
               </div>
               <div className="cardBody">
                 <div className="pokeTypes">
-                  {pokemon.types.map(type => (
-                    <span className="pokeType">{type.type.name}</span>
+                  {pokemon.types.map((type, index) => (
+                    <span key={index} className="pokeType" style={{backgroundColor: `${pokemonTypeColors[pokemon.types[index].type.name]}`}}>{type.type.name}</span>
                   ))}
                 </div>
-                <p className="pokeName">{pokemon.name}</p>
+                <p className="pokeName">{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</p>
               </div>
             </div>
           ))}
         </div>
-        <button className='btn btnPrimary' onClick={handleShowMorePokemon}>Show More</button>
+        <button className='btn btnPrimary showMore' onClick={handleShowMorePokemon}>Show More</button>
       </section>
     </main>
   )
