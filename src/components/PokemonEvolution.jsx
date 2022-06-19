22142f4bf56fdf7f6aaa10105b6b9c873d65ea76
Alyssa Voccia <avoccia@hotmail.com>
2022-06-19 15:51:13 -0400

    Updated pokemon evolution component to link to the other pokemon in the evolution chain

--- src/components/PokemonEvolution.jsx
+++ src/components/PokemonEvolution.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 function PokemonEvolution({ currentPokemon, pokemon, evolutionChain }) {
   const pokemonChain = [];
 
@@ -15,11 +17,11 @@ function PokemonEvolution({ currentPokemon, pokemon, evolutionChain }) {
       <p className="mb-4 text-2xl text-slate-500 font-bold">Evolution Chain</p>
       <ol className="md:flex md:justify-center text-center md:gap-6">
         {pokemonChain.map(pokemon => (
-          <li key={pokemon.name}>
-            <div className="flex flex-col mt-0.5 ml-4 md:ml-0 pb-5 items-center">
-              <h4 className={`${currentPokemon.species.name === pokemon.name ? 'text-slate-700' : 'text-slate-400'} font-semibold text-xl mb-1.5`}>{pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</h4>
+          <li key={pokemon.name} className={`${currentPokemon.species.name === pokemon.name ? 'text-slate-700' : 'text-slate-400'} hover:text-slate-700`}>
+            <Link to={`/pokemon/${pokemon.name}`} className="flex flex-col mt-0.5 ml-4 md:ml-0 pb-5 items-center">
+              <h4 className={`font-semibold text-xl mb-1.5`}>{pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</h4>
               <img className='w-40' src={pokemon.sprite} alt={`${pokemon.name}`} />
-            </div>
+            </Link>
           </li>
         ))}
       </ol>
