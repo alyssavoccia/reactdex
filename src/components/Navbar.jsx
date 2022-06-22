6fd3096c5faf0f4a5acb52f35902191faa744666
Alyssa Voccia <avoccia@hotmail.com>
2022-06-22 07:44:07 -0400

    Updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,9 @@ function Navbar() {
 
   return (
     <header>
-      <nav className='flex flex-wrap space-y-2 items-center justify-around py-4 overflow-hidden'>
+      <nav className='flex flex-wrap space-y-2 mx-8 items-center justify-between py-4 overflow-hidden'>
         <img className='cursor-pointer' src={logo} alt="ReactDex Logo" onClick={() => navigate('/')} />
-        <div className="flex px-2 gap-2 z-20">
-          <input className='text-sm w-60 py-2 p-4 rounded-sm focus:outline-none' type="text" placeholder="Search Pokemon" />
+        <div className="flex px-2 z-20 items-center justify-center">
           <button className="bg-red-500 hover:bg-red-600 text-white border-b-4 border-red-600 hover:border-red-700 px-4 py-2 transition duration-150 ease-in-out text-sm shadow-md">
             Generate Random Pokemon
           </button>
